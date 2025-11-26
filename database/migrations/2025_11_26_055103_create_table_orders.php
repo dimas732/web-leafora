15c1f98463babcfcb6ac937a7f93d257028aa962
AlbiDimas <dimase.albi23@gmail.com>
2025-11-26 12:57:59 +0700

    file migration

---
+++ database/migrations/2025_11_26_055103_create_table_orders.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_code')->nullable();
+            $table->string('cust_name');
+            $table->string('cust_phone');
+            $table->timestamp('order_date');
+            $table->dateTime('pickup_time');
+            $table->decimal('total_price');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_orders');
+    }
+};
